b2b52a49ee1287f4cfce70ecf581960888c8fca3
gary <garygg42@gmail.com>
2013-02-25 16:11:23 +0200

    new client

--- parsestring-demo/README.txt
+++ parsestring-demo/README.txt
@@ -9,12 +9,5 @@ Install example feature in container:
 Starting the client
 -------------------
 in root of this project execute 
-
-	mvn exec:java -pl client
-for simple client, which sends 25 random strings and 1 checked exception to parsestring service
-
-	mvn exec:java -pl client-gui
-for client with GUI
-	
-	mvn exec:java -pl client-many-inv
-for client, which sends 1000 random strings to parsestring service
+	mvn exec:java -pl client -Dcommandline.args="loop"
+for client, which sends random strings in infinity loop to parsestring service
--- parsestring-demo/client/src/main/java/demo/client/StringWrapperClient.java
+++ parsestring-demo/client/src/main/java/demo/client/StringWrapperClient.java
@@ -2,6 +2,7 @@ package demo.client;
 
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -12,6 +13,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import demo.common.StringWrapper;
 import demo.common.StringWrapperService;
 
+@SuppressWarnings("restriction")
 public final class StringWrapperClient {
 
     public static String getRandomString() {
@@ -33,15 +35,86 @@ public final class StringWrapperClient {
         StringWrapperService client = (StringWrapperService) context
                 .getBean("stringWrapperClient");
 
-        ArrayList<String> requests = new ArrayList<String>();
+        if (args[0].equals("25")) {
+            ArrayList<String> requests = new ArrayList<String>();
 
-        for (int i = 0; i < 25; i++) {
-            requests.add(StringWrapperClient.getRandomString());
+            for (int i = 0; i < 25; i++) {
+                requests.add(StringWrapperClient.getRandomString());
+            }
+            System.out.println("#####################################");
+            System.out.println("#Testing service with 25 random words");
+            System.out.println("#####################################");
+            for (String string : requests) {
+                JAXBContext jaxbContext;
+                try {
+                    jaxbContext = JAXBContext.newInstance(StringWrapper.class);
+
+                    Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+
+                    jaxbMarshaller.setProperty(
+                            Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+                    jaxbMarshaller.marshal(client.getParsedString(string),
+                            System.out);
+                } catch (JAXBException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println("#####################################");
+            System.out.println("#Throwing business exception");
+            System.out.println("#####################################");
+            try {
+                client.getParsedString("AAA123");
+            } catch (RuntimeException e) {
+                e.printStackTrace();
+            }
         }
-        System.out.println("#####################################");
-        System.out.println("#Testing service with 25 random words");
-        System.out.println("#####################################");
-        for (String string : requests) {
+
+        else if (args[0].equals("1000")) {
+            ArrayList<String> requests = new ArrayList<String>();
+
+            for (int i = 0; i < 1000; i++) {
+                requests.add(StringWrapperClient.getRandomString());
+            }
+            System.out
+                    .println("#################################################");
+            System.out
+                    .println("#Testing service with 1000 random words (~10 sec)");
+            System.out
+                    .println("#################################################");
+
+            for (String string : requests) {
+                client.getParsedString(string);
+            }
+        }
+
+        else if (args[0].equals("loop")) {
+            while (true) {
+                JAXBContext jaxbContext;
+                try {
+                    jaxbContext = JAXBContext.newInstance(StringWrapper.class);
+
+                    Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+
+                    jaxbMarshaller.setProperty(
+                            Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+                    jaxbMarshaller.marshal(client
+                            .getParsedString(StringWrapperClient
+                                    .getRandomString()), System.out);
+                } catch (JAXBException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        else if (args[0].equals("manual")) {
+            Scanner in = new Scanner(System.in);
             JAXBContext jaxbContext;
             try {
                 jaxbContext = JAXBContext.newInstance(StringWrapper.class);
@@ -51,19 +124,15 @@ public final class StringWrapperClient {
                 jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT,
                         true);
 
-                jaxbMarshaller.marshal(client.getParsedString(string),
-                        System.out);
+                while (true) {
+                    System.out.print("Enter string to parse: ");
+                    String stp = in.nextLine();
+                    jaxbMarshaller.marshal(client.getParsedString(stp),
+                            System.out);
+                }
             } catch (JAXBException e) {
                 e.printStackTrace();
             }
-        }
-        System.out.println("#####################################");
-        System.out.println("#Throwing business exception");
-        System.out.println("#####################################");
-        try {
-            client.getParsedString("AAA123");
-        } catch (RuntimeException e) {
-            e.printStackTrace();
         }
     }
 
